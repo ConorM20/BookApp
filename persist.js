1fa94a41a7ad5d58af329affcf66273decd4d69a
Conor <d00394349@dmail.dixie.edu>
2021-06-25 10:39:07 -0600

    updated persist.js

---
+++ persist.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+function connect(callback) {
+
+    let connectionString = `mongodb+srv://bookapp_2021:2021@cluster0.gfrfr.mongodb.net/bookdatabase?retryWrites=true&w=majority`;
+    console.log("connect to db....");
+}
+
+
