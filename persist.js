fe8b4f9b2f0c0b42f55e9542ae9a379e6053ad7b
Conor <d00394349@dmail.dixie.edu>
2021-06-25 10:55:42 -0600

    persist.js update

--- persist.js
+++ persist.js
@@ -4,6 +4,23 @@ function connect(callback) {
 
     let connectionString = `mongodb+srv://bookapp_2021:2021@cluster0.gfrfr.mongodb.net/bookdatabase?retryWrites=true&w=majority`;
     console.log("connect to db....");
-}
+
+    mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      console.log("There was an error connecting to mongo: ", err);
+    });
+
+  mongoose.connection.once("open", callback);
+};
+
+module.exports = {
+    connect,
+  };
+
+
 
 
